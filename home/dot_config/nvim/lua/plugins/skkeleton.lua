7185f623378ba0a2d73a869f0f47919bcd77dcee
yochidros <yoshiki.myzw@gmail.com>
2024-03-16 13:58:14 +0900

    fix: breaking skkleton

--- home/dot_config/nvim/lua/plugins/skkeleton.lua
+++ home/dot_config/nvim/lua/plugins/skkeleton.lua
@@ -1,31 +1,25 @@
 local M = {
 	"vim-skk/skkeleton",
 	lazy = false,
+	commit = "8d0b7337013e466c578e024b7c8e5b5d297f0a3e", -- kowareteru
 	dependencies = {
 		{
 			"vim-denops/denops.vim",
 			config = function()
-				local home = vim.fn.expand("$HOME")
-				vim.g["denops#deno"] = home .. "/.deno/bin/deno"
+				vim.g["denops#deno"] = "/opt/homebrew/bin/deno"
 			end,
 		},
 	},
 }
 
 function M.config()
-	local dict = "~/Library/Application Support/AquaSKK/"
+	local dict = "~/yaskkserv/"
 	vim.fn["skkeleton#config"]({
 		eggLikeNewline = true,
 		debug = false,
 		immediatelyCancel = false,
 		globalDictionaries = {
-			dict .. "SKK-JISYO.L",
-			dict .. "SKK-JISYO.jinmei",
-			dict .. "SKK-JISYO.geo",
-			dict .. "SKK-JISYO.station",
-			dict .. "SKK-JISYO.propernoun",
-			dict .. "SKK-JISYO.jawiki",
-			dict .. "SKK-JISYO.neologd",
+			dict .. "SKK-JISYO.S",
 			dict .. "SKK-JISYO.emoji.utf8",
 		},
 		sources = {
@@ -38,7 +32,6 @@ function M.config()
 		usePopup = true,
 	})
 	vim.keymap.set({ "i" }, "<C-x>", "<Plug>(skkeleton-toggle)")
-	vim.api.nvim_exec("call skkeleton#register_keymap('input', 'L', 'abbrev')", false)
 end
 
 return M
