acb955b069ff1a4a2183697c48b989c7a376d990
yochidros <yoshiki.myzw@gmail.com>
2024-03-18 22:57:13 +0900

    update: configs

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -86,27 +86,6 @@ function M.config()
 		return
 	end
 
-	local kind_labels_mt = {
-		__index = function(_, k)
-			return k
-		end,
-	}
-	local kind_labels = {}
-	setmetatable(kind_labels, kind_labels_mt)
-	lsp_status.register_progress()
-
-	lsp_status.config({
-		kind_labels = kind_labels,
-	})
-
-	vim.api.nvim_command([[
-      function! LspStatus() abort
-      if luaeval('#vim.lsp.buf_get_clients() > 0')
-        return luaeval("require('lsp-status').status()")
-        endif
-        return ''
-        endfunction
-        ]])
 	require("mason-lspconfig").setup()
 
 	local nvim_s, nvim_lsp = pcall(require, "lspconfig")
@@ -279,7 +258,7 @@ function M.config()
 		capabilities = cap,
 	})
 
-	local servers = { "pylsp", "gopls", "kotlin_language_server", "cssls" }
+	local servers = { "pylsp", "gopls", "kotlin_language_server", "cssls", "gleam" }
 	for _, lsp in ipairs(servers) do
 		nvim_lsp[lsp].setup({
 			on_attach = on_attach,
