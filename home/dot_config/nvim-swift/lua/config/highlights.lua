888ba0c6859e1fe1a29300d8cc493c72273c13f3
yochidros <yoshiki.myzw@gmail.com>
2025-06-19 22:50:10 +0900

    rm: nvim config for swift/ios dev, merged into nvim config.

--- home/dot_config/nvim-swift/lua/config/highlights.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.background = "dark"
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
-
-vim.cmd("let base16colorspace=256") --Access colors present in 256 colorspace
-
-vim.opt.termguicolors = true
--- home/dot_config/nvim-swift/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-	"numToStr/Comment.nvim",
-	event = "VeryLazy",
-}
-function M.config()
-	require("Comment").setup()
-end
-
-return M
