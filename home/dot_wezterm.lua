0ae7230af8099a0dbfa45d73ed811cabb0e4a68d
yochidros <yoshiki.myzw@gmail.com>
2025-12-23 11:02:15 +0900

    wezterm: add nvim zen-mode event.

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -5,6 +5,29 @@ wezterm.on("window-config-reloaded", function(window, pane)
 	window:toast_notification("wezterm", "Configuration Reloaded", nil, 4000)
 end)
 
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
 	if tab.is_active then
 		return {
